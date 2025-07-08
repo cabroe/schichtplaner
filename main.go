56a80163af63c2cb100024fb8b810b90390957aa
Carsten Bröckert <cabroe@gmail.com>
2025-07-08 23:23:47 +0200

    feat: add rate limiting middleware
    This commit adds rate limiting middleware to the echo instance.
    
    - Adds `middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(rate.Limit(20)))` to the `e.Use()` calls in `main.go`.
    - This will limit the number of requests that can be made to the server to 20 requests per second.
    - Imports the `golang.org/x/time/rate` package.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/time/rate"
 )
 
 func main() {
@@ -19,6 +20,7 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Gzip())
 	e.Use(middleware.CORS())
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(rate.Limit(20))))
 	e.Use(echoprometheus.NewMiddleware("schichtplaner"))
 
 	// Setup the frontend handlers to service vite static assets
