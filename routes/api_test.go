d26d770850664d20bf5b31dc676dc5d369262315
Carsten Bröckert <cabroe@gmail.com>
2025-07-16 20:19:53 +0200

    Abhängigkeiten in go.mod aktualisiert: 'testify' auf v1.8.1 und neue indirekte Abhängigkeiten 'go-spew', 'go-difflib' sowie 'yaml' hinzugefügt. go.sum entsprechend angepasst.

---
+++ routes/api_test.go
@@ -0,0 +1,23 @@
+package routes
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetMessageHandler(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/api/message", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	// Handler direkt aufrufen
+	if assert.NoError(t, getMessageHandler(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.JSONEq(t, `{"message": "Hello, from the golang World!"}`, rec.Body.String())
+	}
+}
