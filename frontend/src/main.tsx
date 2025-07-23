f4f0b9ede15d216d499636b22c1f0fcd5c039d65
Carsten Bröckert <cabroe@gmail.com>
2025-07-23 10:10:37 +0200

    Aktualisierung der `package.json` und `yarn.lock`: Upgrade von `@fortawesome/fontawesome-free` auf Version 6, `@tabler/core` auf Version 1.0.0-beta21, sowie Hinzufügen von `autosize`, `imask` und `tom-select`. Anpassung der Vite-Konfiguration zur Verbesserung der SCSS-Verarbeitung und Stummschaltung von Warnings. Importanpassungen in `main.tsx` zur Verwendung von SCSS und aktualisierten Tabler-Ressourcen.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import '@tabler/core/dist/css/tabler.min.css'
-import '@tabler/core/dist/js/tabler.min.js'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import './index.css'
+import './assets/sass/_app.scss'
+import '@tabler/core/src/js/tabler.js'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        verbose: false,
+        logger: {
+          warn: () => {} // Alle Warnings stumm schalten
+        }
+      },
+    },
+  },
   build: {
     target: 'esnext',
     minify: 'terser',
